a3a20eb7a79558c7e25176af46b47a0c44bd3d1e
Henry Campos <henry.camposnavarro@ucr.ac.cr>
2024-09-04 22:36:40 -0600

    Se suben los archivos de codigo del laboratorio 4 al GitHub del curso

---
+++ Laboratorio4/frontend-lab/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <ListaPaises />
+</template>
+
+<script>
+import ListaPaises from './components/ListaPaises.vue';
+
+/* npm run serve  para correr dentro de la carpeta del frontend-lab */
+
+export default {
+  name: 'App',
+  components: {
+    ListaPaises
+   },
+};
+</script>
+
+<style>
+#app {
+
+}
+</style>
