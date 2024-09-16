1a1a400f65b3049cf9f228b67524b5b87a51be41
Henry Campos <henry.camposnavarro@ucr.ac.cr>
2024-09-16 14:56:31 -0600

    Laboratorio#6: Se crea la carpeta para el lab 6 con los dos laboratorios anteriores y se completa el codigo segun la guia brindada

---
+++ Laboratorio6/backend-lab-C21636/backend-lab-C21636/Models/PaisModel.cs
@@ -0,0 +1,13 @@
+﻿namespace backend_lab_C21636.Models
+{
+    public class PaisModel
+    {
+        public int Id { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Continente { get; set; }
+
+        public string Idioma { get; set;}
+    }
+}
---
+++ Laboratorio6/frontend-lab/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <router-view></router-view>
+</template>
+
+<script>
+
+
+/* npm run serve  para correr dentro de la carpeta del frontend-lab */
+</script>
+
+<style>
+#app {
+
+}
+</style>
